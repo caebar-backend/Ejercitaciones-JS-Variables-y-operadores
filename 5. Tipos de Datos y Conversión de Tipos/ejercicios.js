c595812c059a9d4ad94edb237cd53c152424d100
[caebar-backend] <[caebar.cursos.backend.zarate@gmail.com]>
2025-03-28 19:39:45 -0300

    Punto 5 Finalizado

--- 5. Tipos de Datos y Conversión de Tipos/ejercicios.js
+++ 5. Tipos de Datos y Conversión de Tipos/ejercicios.js
@@ -1,2 +1,10 @@
 // Ejercicio 5.1: Declara una variable 'cadenaNumero' y asígnale el valor "123". Convierte esta cadena a un número y almacénalo en una nueva variable 'numeroConvertido'. Muestra ambos valores en la consola.
+let cadenaNumero = "123"
+const numeroConvertido = Number(cadenaNumero)
+console.log(`El valor de la variable "cadenaNumero" es ${cadenaNumero}, y el valor de la constante "numeroConvertido" es ${numeroConvertido}`) // 123 123
+
+
 // Ejercicio 5.2: Declara una variable 'booleano' y asígnale el valor 'true'. Convierte este valor a una cadena y almacénalo en una nueva variable 'cadenaBooleano'. Muestra ambos valores en la consola.
+let booleano = true
+const cadenaBooleano = String(booleano)
+console.log(`El valor de la variable "booleano" es ${booleano}, y el valor de la constante "cadenaBooleano" es ${cadenaBooleano}`) // true true
