892ed055272fe35f9859d3158276adcd8993a57f
[caebar-backend] <[caebar.cursos.backend.zarate@gmail.com]>
2025-03-28 19:10:58 -0300

    Punto 1 Finalizado

--- 1. Declaración y Asignación de Variables/ejercicios.js
+++ 1. Declaración y Asignación de Variables/ejercicios.js
@@ -1,3 +1,9 @@
 // Ejercicio 1.1: Declara una variable llamada 'nombre' y asígnale tu nombre.
+const nombre = "Cristian"
+let primerNombre = "Cristian"
 // Ejercicio 1.2: Declara una variable llamada 'edad' y asígnale tu edad.
+let edad = 42
+const años = 42
 // Ejercicio 1.3: Declara una variable llamada 'esEstudiante' y asígnale el valor 'true' si eres estudiante o 'false' si no lo eres.
+let esEstudiante = false
+const esEstudianteS = true
