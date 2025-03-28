c581999a1172dd08e6431e8a5b9eb66a174045ba
[caebar-backend] <[caebar.cursos.backend.zarate@gmail.com]>
2025-03-28 19:27:32 -0300

    Punto 3 Finalizado

--- 3. Concatenación de Cadenas/ejercicios.js
+++ 3. Concatenación de Cadenas/ejercicios.js
@@ -1,1 +1,7 @@
 // Ejercicio 3.1: Declara dos variables, 'saludo' y 'nombre', y asígnales los valores "Hola" y tu nombre respectivamente. Usa el operador de concatenación para unir estas dos cadenas y muestra el resultado en la consola.
+let saludo = "Hola"
+const nombre = "Cristian"
+
+console.log(`-- Resultado de la operación`)
+console.log(saludo + ', ' + nombre)
+console.log(`${saludo}, ${nombre}`)
