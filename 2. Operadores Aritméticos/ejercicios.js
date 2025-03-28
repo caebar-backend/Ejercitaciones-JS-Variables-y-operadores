f5f8fc974c9cbad081b4c35fee7db0e4f8d769df
[caebar-backend] <[caebar.cursos.backend.zarate@gmail.com]>
2025-03-28 19:19:40 -0300

    Punto 2 Finalizado

--- 2. Operadores Aritméticos/ejercicios.js
+++ 2. Operadores Aritméticos/ejercicios.js
@@ -1,6 +1,19 @@
 // Ejercicio 2.1: Declara dos variables, 'a' y 'b', asígnales los valores 5 y 3 respectivamente. Calcula y muestra en la consola el resultado de:
+let a = 5
+const b = 3
+console.log(`-- El valor de a es ${a}, y el valor de b es ${b}`)
 // - La suma de 'a' y 'b'.
+console.log(`-- La suma de a y b es ${a + b}`)
+console.log(a + b)
 // - La resta de 'a' y 'b'.
+console.log(`-- La resta de a y b es ${a - b}`)
+console.log(a - b)
 // - La multiplicación de 'a' y 'b'.
+console.log(`-- La multiplicación de a y b es ${a * b}`)
+console.log(a * b)
 // - La división de 'a' entre 'b'.
+console.log(`-- La división de a entre b es ${a / b}`)
+console.log(a / b)
 // - El módulo de 'a' y 'b'.
+console.log(`-- El módulo de a y b es ${a % b}`)
+console.log(a % b)
