e7dd94a50894e5597ae0e007aa9bf4929afe0f86
[caebar-backend] <[caebar.cursos.backend.zarate@gmail.com]>
2025-03-28 19:33:01 -0300

    Punto 4 Finalizado

--- 4. Operadores de Asignación/ejercicios.js
+++ 4. Operadores de Asignación/ejercicios.js
@@ -1,6 +1,23 @@
 // Ejercicio 4.1: Declara una variable 'x' y asígnale el valor 10. Usa los operadores de asignación compuesta para realizar las siguientes operaciones:
+let x = 10
+console.log(`--- El valor de x es: ${x}`)
+
 // - Suma 5 a 'x'.
+x += 5
+console.log(`--- El valor de x después de sumar 5 es: ${x}`)
+
 // - Resta 3 de 'x'.
+x -= 3
+console.log(`--- El valor de x después de restar 3 es: ${x}`)
+
 // - Multiplica 'x' por 2.
+x *= 2
+console.log(`--- El valor de x después de multiplicar por 2 es: ${x}`)
+
 // - Divide 'x' entre 4.
+x /= 4
+console.log(`--- El valor de x después de dividir entre 4 es: ${x}`)
+
 // - Calcula el módulo de 'x' con 3.
+x %= 3
+console.log(`--- El valor de x después de calcular el módulo con 3 es: ${x}`)
